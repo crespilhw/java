f1d9337fc258d79678d6b42f6ea1f32997a7e3d1
crespilhw <gustavocrespilho1a@gmail.com>
2025-03-26 15:51:48 -0300

    idade

---
+++ Idade.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Idade
+{
+	public static void main(String[] args) {
+	    int idade;
+		Scanner ler = new Scanner(System.in);
+		
+		System.out.println("Insira sua idade: ");
+		idade = ler.nextInt();
+		
+		if (idade >= 18)
+		System.out.println("Maior de idade");
+		else
+		System.out.println("Menor de idade");
+	}
+}
