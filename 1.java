4e2e80daa6e7f8f434ff30dcbdb7ddcd77cf60eb
crespilhw <gustavocrespilho1a@gmail.com>
2025-03-31 07:54:50 -0300

    Update and rename Media.java to 1.java

--- 1.java
+++ 1.java
@@ -3,17 +3,15 @@ public class Media {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
         
-        int n1, n2, n3, soma, media;
+        int n1, n2, soma, media;
         
         System.out.print("Informe primeira nota: ");
         n1 = ler.nextInt();
         System.out.print("Informe segunda nota: ");
         n2 = ler.nextInt();
-        System.out.print("Informe terceira nota: ");
-        n3 = ler.nextInt();
-        
-        soma = n1 + n2 + n3;
-        media = soma / 3;
+      
+        soma = n1 + n2;
+        media = soma / 2;
         
         if (media >= 7)
         System.out.println("O aluno passou!");
