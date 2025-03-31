e112213a795e5def99821df4035b02de3eb8e32a
crespilhw <gustavocrespilho1a@gmail.com>
2025-03-31 15:50:14 -0300

    Ex14.java

---
+++ Ex14.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Ex14
+{
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        for(int i=1 ; i <= 10; i++)
+        System.out.println("Eu gosto de estudar algoritmos!");
+    }
+}
