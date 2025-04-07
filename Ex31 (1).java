0c60e63ddfefcf26cc7f4eec912e835c61310b8d
crespilhw <gustavocrespilho1a@gmail.com>
2025-04-07 15:46:39 -0300

    Ex31

---
+++ Ex31 (1).java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Ex31
+{
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+       int t = 1;
+  
+        while (t <= 19) {
+            t++;
+            System.out.print(t + " "); }
+        
+        
+    }
+}
