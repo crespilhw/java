aca43854b39bc117e19adc2a7868f4945bde6dce
crespilhw <gustavocrespilho1a@gmail.com>
2025-04-07 15:44:34 -0300

    Ex25

---
+++ Ex25.java
@@ -0,0 +1,12 @@
+public class Ex25
+{
+	public static void main(String[] args) {
+	    int numero = 0;
+	    
+	   while (numero <= 99) {
+	       numero++;
+	System.out.println("Número: " + numero);
+	}
+
+	}
+}
