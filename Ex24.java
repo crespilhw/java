30680f6e95449ce5676a69cb0271aded80db2459
crespilhw <gustavocrespilho1a@gmail.com>
2025-04-07 10:43:34 -0300

    ex24

---
+++ Ex24.java
@@ -0,0 +1,13 @@
+public class Ex24
+{
+	public static void main(String[] args) {
+	    int soma = 0;
+	    int numero = 1;
+	    
+	    while (numero <= 10) {
+	   soma += numero;
+	   numero++;
+	}
+	System.out.println("A soma dos número de 1 a 10 é: " + soma);
+	}
+}
