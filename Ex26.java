412c64ea039a2f422c72eba875cbd8f94b00db6e
crespilhw <gustavocrespilho1a@gmail.com>
2025-04-07 15:45:04 -0300

    Ex26

---
+++ Ex26.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Ex26
+{
+	public static void main(String[] args) {
+	    Scanner ler = new Scanner (System.in);
+	    int n, numero = 0; 
+	    
+	    System.out.println("Insira um número: ");
+	    n = ler.nextInt();
+	    
+	while (numero <= n) {
+	numero++;
+	System.out.println(numero);
+	        
+	    }
+	}
+}
