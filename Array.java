a57096d9c79484258ff5ea633a3b342fbd7c7d54
Aluno <gustavocrespilho1a@gmail.com>
2025-04-14 15:16:38 -0300

    array

---
+++ Array.java
@@ -0,0 +1,16 @@
+public class Array
+{
+    public static void main(String[] args) {
+        int[] numeros = new int [5];
+
+        numeros[0] = 10;
+        numeros[1] = 20;
+        numeros[2] = 30;
+        numeros[3] = 40;
+        numeros[4] = 50;
+
+        for(int i=0; i <numeros.length; i++){
+            System.out.println("O " + (i+1) + "° número é: " + numeros[i]);
+        }
+        }
+}
