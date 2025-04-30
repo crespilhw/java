6f430813abed83155eec3093778778772bc2646d
crespilhw <gustavocrespilho1a@gmail.com>
2025-04-30 07:21:36 -0300

    001

---
+++ Ex001.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Ex001
+{
+	public static void main(String[] args) {
+	    Scanner ler = new Scanner(System.in);
+	    int n;
+		System.out.println("Informe um número: ");
+		n = ler.nextInt();
+		System.out.println("O número informado foi: " + n);
+	}
+}
