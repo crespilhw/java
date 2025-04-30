908b060caac2208e09827ea30d839b64d536a90a
crespilhw <gustavocrespilho1a@gmail.com>
2025-04-30 09:26:50 -0300

    013

---
+++ Ex013.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Ex013
+{
+	public static void main(String[] args) {
+	    Scanner ler = new Scanner(System.in);
+	    int n1, n2, n3;
+		System.out.println("Insira o 1º número: ");
+		n1 = ler.nextInt();
+		System.out.println("Insira o 2º número: ");
+		n2 = ler.nextInt();
+		System.out.println("Insira o 3º número: ");
+		n3 = ler.nextInt();
+		
+		if (n1 > n2 && n1 > n3){
+		    System.out.println("O primeiro número é o maior.");
+		}
+     	else if (n2 > n1 && n2 > n3){
+		    System.out.println("O segundo número é o maior.");
+		}
+		else if (n3 > n2 && n3 > n1){
+		    System.out.println("O segundo número é o maior.");
+		}
+	}
+}
