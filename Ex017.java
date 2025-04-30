23925862a971414c5d42dc648c5b7e5a243dbc4c
crespilhw <gustavocrespilho1a@gmail.com>
2025-04-30 10:38:08 -0300

    Ex017.java

--- Ex017.java
+++ Ex017.java
@@ -3,7 +3,7 @@ public class Ex017
 	public static void main(String[] args) {
 		System.out.println("Os números ímpares entre 1 e 50: ");
 		for (int i = 1; i <= 50; i++) {
-		if (i % 2 == 0){
+		if (i % 2 != 0){
 		    System.out.println(i);
 		}
 	
