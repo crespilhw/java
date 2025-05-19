c481715080e31879f51cab089a41c67cb325a23d
crespilhw <gustavocrespilho1a@gmail.com>
2025-05-19 07:33:16 -0300

    Matriz01.java

--- Matriz01.java
+++ Matriz01.java
@@ -1,4 +1,4 @@
-public class Matriz
+public class Matriz01
 {
     public static void main(String[] args) {
         int[][] matriz = new int[4][4];
@@ -9,18 +9,22 @@ public class Matriz
         matriz[0][0] = 5;
         matriz[0][1] = 3;
         matriz[0][2] = 2;
+        matriz[0][3] = 1;
 
         matriz[1][0] = 4;
         matriz[1][1] = 3;
         matriz[1][2] = 6;
+        matriz[1][3] = 2;
 
         matriz[2][0] = 7;
         matriz[2][1] = 8;
         matriz[2][2] = 1;
+        matriz[2][3] = 5;
         
         matriz[3][0] = 9;
         matriz[3][1] = 6;
         matriz[3][2] = 3;
+        matriz[3][3] = 2;
         
         System.out.println("Número de linhas: " + numLinhas);
         System.out.println("Número de colunas: " + numColunas);
